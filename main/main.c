819787ab386942b95a0418454cbf94e9927cc678
ky-z03 <1139187496@qq.com>
2025-10-22 15:28:50 +0800

    XL9555

--- main/main.c
+++ main/main.c
@@ -4,22 +4,59 @@
 #include "nvs_flash.h"
 #include "esp_log.h"
 #include "LED.h"
-#include "PWM.h"
+#include "IIC.h"
+#include "XL9555.h"
+
+i2c_obj_t i2c0_master;
 
 void app_main(void)
 {
+    uint8_t key;
     esp_err_t ret;
-    ret = nvs_flash_init();/* 初始化 NVS */
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
-    ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    
+    ret = nvs_flash_init();             /* 初始化NVS */
+
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
     {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
-    pwm_init(13, 5000); /* 初始化 PWM */
-    while (1)
+
+    led_init();                         /* 初始化LED */
+    i2c0_master = iic_init(I2C_NUM_0);  /* 初始化IIC0 */
+    xl9555_init(i2c0_master);           /* 初始化XL9555 */
+
+    while(1)
     {
-        vTaskDelay(10);
-        pwm_set_duty(LEDC_PWM_DUTY);
+        key = xl9555_key_scan(0);
+        
+        switch (key)
+        {
+            case KEY0_PRES:
+            {
+                xl9555_pin_write(BEEP_IO, 0);
+                break;
+            }
+            case KEY1_PRES:
+            {
+                xl9555_pin_write(BEEP_IO, 1);
+                break;
+            }
+            case KEY2_PRES:
+            {
+                LED_show();
+                break;
+            }
+            case KEY3_PRES:
+            {
+                LED_close();
+                break;
+            }
+            default:
+            {
+                break;
+            }
+        }
+        vTaskDelay(200);
     }
 }
