429c5b0953e53b3960c54e59ed11d9f04c74d50c
ky-z03 <1139187496@qq.com>
2025-10-23 10:26:49 +0800

    ADC

--- main/main.c
+++ main/main.c
@@ -2,21 +2,22 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "nvs_flash.h"
-#include "led.h"
+#include "LED.h"
 #include "IIC.h"
 #include "XL9555.h"
-#include "SPI.h"
 #include "LCD.h"
+#include "ADC1.h"
+
 
 i2c_obj_t i2c0_master;
 
 void app_main(void)
 {
-    uint8_t x = 0;
+    uint16_t adcdata;
+    float voltage;
     esp_err_t ret;
-    
-    
-    ret = nvs_flash_init();             /* 初始化NVS */
+       
+    ret = nvs_flash_init();                                     /* 初始化NVS */
 
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
     {
@@ -24,90 +25,34 @@ void app_main(void)
         ret = nvs_flash_init();
     }
 
-    led_init();                         /* 初始化LED */
-    i2c0_master = iic_init(I2C_NUM_0);  /* 初始化IIC0 */
-    spi2_init();                        /* 初始化SPI2 */
-    xl9555_init(i2c0_master);           /* IO扩展芯片初始化 */
-    lcd_init();                         /* 初始化LCD */
+    led_init();                                                 /* 初始化LED */
+    i2c0_master = iic_init(I2C_NUM_0);                          /* 初始化IIC0 */
+    spi2_init();                                                /* 初始化SPI2 */
+    xl9555_init(i2c0_master);                                   /* 初始化XL9555 */
+    lcd_init();                                                 /* 初始化LCD */
+    adc_init();                                                 /* 初始化ADC */
 
-    while (1)
+    lcd_show_string(10, 50, 200, 16, 16, "ESP32", RED);
+    lcd_show_string(10, 70, 200, 16, 16, "ADC TEST", RED);
+    lcd_show_string(10, 90, 200, 16, 16, "ATOM@ALIENTEK", RED);
+    
+    lcd_show_string(10, 110, 200, 16, 16, "ADC1_CH7_VAL:", BLUE);
+    lcd_show_string(10, 130, 200, 16, 16, "ADC1_CH7_VOL:     V", BLUE);
+    
+    while(1)
     {
-        switch (x)
-        {
-            case 0:
-            {
-                lcd_clear(WHITE);
-                break;
-            }
-            case 1:
-            {
-                lcd_clear(BLACK);
-                break;
-            }
-            case 2:
-            {
-                lcd_clear(BLUE);
-                break;
-            }
-            case 3:
-            {
-                lcd_clear(RED);
-                break;
-            }
-            case 4:
-            {
-                lcd_clear(MAGENTA);
-                break;
-            }
-            case 5:
-            {
-                lcd_clear(GREEN);
-                break;
-            }
-            case 6:
-            {
-                lcd_clear(CYAN);
-                break;
-            }
-            case 7:
-            {
-                lcd_clear(YELLOW);
-                break;
-            }
-            case 8:
-            {
-                lcd_clear(BRRED);
-                break;
-            }
-            case 9:
-            {
-                lcd_clear(GRAY);
-                break;
-            }
-            case 10:
-            {
-                lcd_clear(LGRAY);
-                break;
-            }
-            case 11:
-            {
-                lcd_clear(BROWN);
-                break;
-            }
-        }
+        adcdata = adc_get_result_average(ADC_ADCX_CHY, 10);
+        lcd_show_xnum(114, 110, adcdata, 5, 16, 0, BLUE);       /* 显示ADC采样后的原始值 */
 
-        lcd_show_string(10, 40, 240, 32, 32, "ESP32", RED);
-        lcd_show_string(10, 80, 240, 24, 24, "SPILCD TEST", RED);
-        lcd_show_string(10, 110, 240, 16, 16, "ATOM@ALIENTEK", RED);
-        lcd_show_string(10, 150, 240, 12, 12, "ATOM@ALIENTEK", RED);
-        x++;
-
-        if (x == 12)
-        {
-            x = 0;
-        }
-
-        LED_TOGGLE();
-        vTaskDelay(500);
+        voltage = (float)adcdata * (3.3 / 4095);                /* 获取计算后的带小数的实际电压值 */
+        adcdata = voltage;                                      /* 赋值整数部分给adcx变量 */
+        lcd_show_xnum(114, 130, adcdata, 1, 16, 0, BLUE);       /* 显示电压值的整数部分 */
+        
+        voltage -= adcdata;                                     /* 把已经显示的整数部分去掉，留下小数部分 */
+        voltage *= 1000;                                        /* 小数部分乘以1000 */
+        lcd_show_xnum(130, 130, voltage, 3, 16, 0x80, BLUE);    /* 显示小数部分 */
+    
+        LED_TOGGLE();                                           /* LED翻转函数 */
+        vTaskDelay(100);
     }
 }
